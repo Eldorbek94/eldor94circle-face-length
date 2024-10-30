1adc806fbe4cb60b70d9a52a3bc402b4ae72c6e2
Eldorbek94 <eldorbtc@gmail.com>
2024-10-30 15:24:59 +0500

    init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+Console.WriteLine("Calculate circle face and length \n Radius di kiriting R:");
+string rad = Console.ReadLine();
+int radius = Convert.ToInt32(rad);
+decimal pi = (decimal)Math.PI;
+
+//calculate Circle's face
+decimal S= pi*radius*radius;
+//calculate Circle's Length
+decimal L = 2*pi*radius;
+
+Console.WriteLine($"This is S:{S}");
+Console.WriteLine($"This is L:{L}");
